8ce5b9fbc3685de8fb3b034019149ed0879eb809
pgtoopx <eduardoinbrand@gmail.com>
2023-03-11 01:23:51 +0000

    a

--- popup.js
+++ popup.js
@@ -1,23 +1,23 @@
-const transcriptToggle = document.querySelector('[data-purpose="transcript-toggle"]');
-const copyButton = document.querySelector('#copyButton');
-const inputElement = document.querySelector('#input-text');
-
-copyButton.addEventListener('click', function() {
-  // Hacer clic en el botón para mostrar el div
-  transcriptToggle.click();
-  
-  // Esperar un tiempo para que el div aparezca
-  setTimeout(function() {
-    // Obtener el texto del div y establecerlo en la entrada de texto
-    const transcriptDiv = document.querySelector('.transcript--transcript-panel—kfMxM');
-    const transcriptText = transcriptDiv.textContent;
-    inputElement.value = transcriptText;
-    inputElement.focus();
+document.addEventListener('DOMContentLoaded', function() {
+    const transcriptToggle = document.querySelector('[data-purpose="transcript-toggle"]');
+    const copyButton = document.querySelector('#copyButton');
     
-    // Solicitar al usuario que confirme el texto a enviar
-    const promptResult = window.prompt('Texto a enviar:', transcriptText);
-    if (promptResult !== null) {
-      inputElement.value = promptResult;
-    }
-  }, 2000);
-});
+    copyButton.addEventListener('click', function() {
+      // Hacer clic en el botón para mostrar el div
+      transcriptToggle.click();
+      
+      // Esperar un tiempo para que el div aparezca
+      setTimeout(function() {
+        // Obtener el texto del div y copiarlo al portapapeles
+        const transcriptDiv = document.querySelector('.transcript--transcript-panel—kfMxM');
+        const transcriptText = transcriptDiv.textContent;
+        const tempTextarea = document.createElement('textarea');
+        tempTextarea.value = transcriptText;
+        document.body.appendChild(tempTextarea);
+        tempTextarea.select();
+        document.execCommand('copy');
+        document.body.removeChild(tempTextarea);
+      }, 2000);
+    });
+  });
+  
